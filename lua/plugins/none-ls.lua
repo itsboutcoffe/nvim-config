1ebd56e9075f7c359c82b4775ee3b53652f935ee
Buddha Shrestha <120118166+itsboutcoffe@users.noreply.github.com>
2025-05-08 21:58:50 +0545

    Update none-ls.lua

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,7 @@ return {
 				--null_ls.builtins.diagnostics.rubocop,
 				--null_ls.builtins.formatting.rubocop,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint,
+				--null_ls.builtins.diagnostics.eslint,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.clangd,
